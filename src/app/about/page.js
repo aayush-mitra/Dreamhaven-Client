1654037b7da4a79df9e66c9de617fa5021f6ee05
Aayush Mitra <aayush.mitra121@gmail.com>
2025-01-28 09:35:52 -0600

    fixed about

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+
+import {redirect} from 'next/navigation'
+import { signIn } from "next-auth/react"
+import Link from 'next/link'
+import {useState, useEffect} from 'react'
+
+export default function About() {
+    return (
+        <h1>About</h1>
+    )
+}
